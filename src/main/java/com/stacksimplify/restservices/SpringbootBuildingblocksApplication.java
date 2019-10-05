393a06842d462d00467df25f67fd1aa6b30d2d41
external.oliver.ruess@kuehne-nagel.com <external.oliver.ruess@kuehne-nagel.com>
2019-10-05 19:22:54 +0200

    3

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
-//
+// Test
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
 	}
